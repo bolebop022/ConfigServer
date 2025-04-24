5ef79552e77cb5f0bdc2b03627a6c0eff21ee5a4
Bongane Lebopo <bonganelebop@gmail.com>
2025-04-24 16:00:59 +0200

    Created response message.

--- src/main/java/org/blindustries/ConfigHandler.java
+++ src/main/java/org/blindustries/ConfigHandler.java
@@ -4,10 +4,27 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class ConfigHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        String path = exchange.getRequestURI().getPath();
+        String[] pathParts = path.split("/");
 
+        // Handle when path returned is invalid
+        if (pathParts.length < 3){
+            sendResponse(exchange, 400, "Invalid request. Use /config/{appName}");
+            return;
+        }
+    }
+
+    private void sendResponse(HttpExchange exchange, int statusCode, String response) throws IOException {
+        byte[] responseBytes = response.getBytes(StandardCharsets.UTF_8);
+        exchange.sendResponseHeaders(statusCode,responseBytes.length);
+        try (OutputStream os = exchange.getResponseBody()){
+            os.write(responseBytes);
+        }
     }
 }
